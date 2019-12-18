9f56174b27ac055c0e1d09c4ae7f911fd40915a1
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2019-12-18 10:50:12 -0500

    gstd: Added all to GSTD namespace

--- gstd.hpp
+++ gstd.hpp
@@ -4,17 +4,21 @@
 #include <vector>
 #include <string>
 
-/*
- Struct containing a string and a size_t. Designed to contain a subtring
- and it's start index in the original string.
- */
-typedef struct{
-    std::string str;
-    size_t idx;
-}string_idx;
+namespace gstd{
 
-std::vector<std::string> parse(std::string input, std::string delin);
-std::vector<std::string_idx> parseIdx(std::string input, std::string delin);
-void ensure_whitespace(string& in, string targets);
+    /*
+     Struct containing a string and a size_t. Designed to contain a subtring
+     and it's start index in the original string.
+     */
+    typedef struct{
+        std::string str;
+        size_t idx;
+    }string_idx;
+
+    std::vector<std::string> parse(std::string input, std::string delin);
+    std::vector<string_idx> parseIdx(std::string input, std::string delin);
+    void ensure_whitespace(std::string& in, std::string targets);
+
+}
 
 #endif
